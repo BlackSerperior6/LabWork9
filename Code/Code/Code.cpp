891840c4e8e6ed134abdf6a80062b7ad711287be
BlackSerperior6 <m330.nek76@gmail.com>
2024-02-18 17:23:14 +0500

    Блок схемы + финальные правки в код

--- Code/Code/Code.cpp
+++ Code/Code/Code.cpp
@@ -4,30 +4,45 @@
 
 using namespace std;
 
-char vowels[] = {'a', 'А', 'у', 'У', 'о', 'О', 'ы', 'и'};
+//Массив всех гласных букв русского и английского алфавитов
+char vowels[] = {'а', 'А', 'е', 'Е', 'ё', 'Ё', 'э', 'Э', 'у', 'У', 'о', 'О', 'ы', 'и', 'И', 'ю', 'Ю', 'я', 'Я', 'a', 'A', 'o', 'O', 'e', 'E', 'i', 'I', 'u', 'U', 'y', 'Y'};
 
-bool CheckForReapets(string *Arr, string element, int index, int size) 
+bool CheckForReapets(string *Arr, string element, int index, int size) //Функция поверки элемента массива на повторение
+{
+	bool flag = false; //Флаг
+	int counter = index + 1; //Счетчик, проверки ведем со следующего элемента
+
+	while (counter < size && !flag) //Пока не нашли повтор или же пока не достигли конца массива
+	{
+		flag = element == Arr[counter]; //Проверить элемент на совпадение
+		counter++; //Увеличить счетчик
+	}
+
+	return flag; //Вернуть флаг
+};
+
+bool IsVowel(char symbol)  //Проверка символа на гласную букву, логика аналогична функции выше
 {
 	bool flag = false;
-	int counter = index + 1;
+	int counter = 0;
 
-	while (counter < size && !flag)
+	while (counter < 31 && !flag)
 	{
-		flag = element == Arr[counter];
+		flag = symbol == vowels[counter];
 		counter++;
 	}
 
 	return flag;
-};
+}
 
-bool CheckLine(string String) 
+bool CheckLine(string String)  //Функция на проверку строки на наличие повторяющихся слов
 {
-	string Arr[500];
-	int counter = 0;
-	int size = String.size();
-	int ArrOfStringsCounter = 0;
-	string buffer = "";
-	bool flag = false;
+	string Arr[500]; //Массив строк
+	int ArrOfStringsCounter = 0; //Счетчик массива
+	int counter = 0; //Счетчик
+	int size = String.size(); //Размер строки
+	string buffer = ""; //Буффер 
+	bool flag = false; //Флаг
 
 	for (int i = 0; i < size + 1; i++) //Проходим по всей строке
 	{
@@ -41,50 +56,77 @@ bool CheckLine(string String)
 			buffer += String[i]; //Иначе заносим в буффер текущий символ
 	}
 
-	while (counter < size && !flag)
+	while (counter < ArrOfStringsCounter && !flag) //Пока не дошли до конца массива строк или же пока не нашли повтор
 	{
-		flag = CheckForReapets(&Arr[0], Arr[counter], counter, ArrOfStringsCounter);
-		counter++;
+		if (Arr[counter].size() > 1) //Если строка по счетчику в массиве по размеру больше единицы
+			flag = CheckForReapets(&Arr[0], Arr[counter], counter, ArrOfStringsCounter); //То проверяем её на повтор
+
+		counter++; //Увеличиваем счетчик
 	}
 
-	return flag;
+	return flag; //Возвращаем флаг
 }
 
 int main()
 {
-	setlocale(LC_ALL, "rus");
+	setlocale(LC_ALL, "rus"); //Подключаем русский язык
 
-	ifstream F1("F1.txt");
+	//Открываем файлы
+	ifstream F1("F1.txt"); 
 	ofstream F2("F2.txt");
+
+	//Проверка на то, что файлы открылись
+	if (!F1.is_open() || !F2.is_open())
+	{
+		cout << "Не найдены нужные файлы" << endl;
+		return 0;
+	}
 
 	string buffer;
 
+	//Пока в F1 есть, что читать
 	while (getline(F1, buffer))
 	{
-		if (CheckLine(buffer))
-			F2 << buffer << endl;
+		if (CheckLine(buffer)) //Если есть одинаковые слова
+			F2 << buffer << endl; //Записать в F2
 	}
 
+	//Закрыть оба файла
 	F1.close();
 	F2.close();
 
+	//Открыть второй файл в качестве входного потока
 	ifstream F3("F2.txt");
 
-	string F3ByLines[500];
-	int ArrayCounter;
-	
-
-	while (getline(F3, buffer))
+	//Если фаил пустой, то закончить программу
+	if (F3.peek() == EOF)
 	{
-		F3ByLines[ArrayCounter] = buffer;
-		ArrayCounter++;
+		cout << "В файле F2 не было ничего передано" << endl;
+		return 0;
 	}
 
-	for (int i = 0; i < F3ByLines[ArrayCounter].size() + 1; i++)
+	cout << "Успешно записали все нужные строки в фаил F2" << endl;
+
+	string F3ByLines[500]; //Переменная массива строк
+	int ArrayCounter = 0; //Счетчик массива
+	int vowelsCounter = 0; //Счетчик кол-ва гласных букв
+
+	while (getline(F3, buffer)) //Пока в третьем файле есть, что читать
 	{
-
-
+		F3ByLines[ArrayCounter] = buffer; //Добавить в массив строку
+		ArrayCounter++; //Увеличить счетчик
 	}
 
+	string lastLine = F3ByLines[ArrayCounter - 1]; //Получаем последний элемент массива строк (будет равен последней строке F2)
+
+	for (int i = 0; i < lastLine.size() + 1; i++) //Пока не достигли последней строки
+	{
+		if (IsVowel(lastLine[i])) //Если встретилась гласная буква
+			vowelsCounter++; //Увеличить счетчик гласных
+	}
+
+	cout << "Кол-во гласных букв в последней строке файла F2: " << vowelsCounter << endl;
+
+	//Закрываем фаил F2
 	F3.close();
 }
